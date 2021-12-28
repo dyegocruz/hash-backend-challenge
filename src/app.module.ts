e260c37043965c82d4c353545a6b75943fb94914
Dyego Cruz <dyegocruz@gmail.com>
2021-12-28 10:09:40 -0300

    - Adicionando módulos Cart e Product.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CartModule } from './cart/cart.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [CartModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
