6d79262a84052fd7782c40165b0936240c5cbf48
Dyego Cruz <dyegocruz@gmail.com>
2021-12-30 16:06:43 -0300

    - Alteração do conteúdo da página inicial.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CartModule } from './cart/cart.module';
 import { ProductModule } from './product/product.module';
 import { DiscountModule } from './discount/discount.module';
@@ -9,6 +8,5 @@ import { DiscountModule } from './discount/discount.module';
 @Module({
   imports: [ConfigModule.forRoot(), CartModule, ProductModule, DiscountModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
